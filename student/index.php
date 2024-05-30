42ae5be4aeec4a1ddaa4bc6f4f5ac03ab80da6d2
kaneki09999 <tanjirowkamado@gmail.com>
2024-05-30 09:23:30 +0800

    sda

--- student/index.php
+++ student/index.php
@@ -9,8 +9,9 @@
             <p>&copy; 2024 Enrollment System. All rights reserved.</p>
         </div>
     </footer>
+    <!-- jsdhaksjdhasjd -->
 
-    <!-- Add Bootstrap JS -->
+    <!--  bootstrap -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.amazonaws.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
