9eff30d226306d70df5e38c9810a7ed2d5d6487e
kaneki09999 <tanjirowkamado@gmail.com>
2024-05-31 08:06:52 +0800

    new

---
+++ admin/include/sidebar.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Enrollment</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<body>
+<div class="top-bar">
+    <div class="container">
+        <p class="contact-info">
+            <span class="enrollment-text">Enrollment System</span>
+        </p>
+    </div>
+</div>
+<div class="sidebar">
+<h2>Admin Dashboard</h2>
+    <ul>
+        <li><a href="index.php"><i class="fas fa-home"></i> Home</a></li>
+        <li><a href="new-enrollees.php"><i class="fas fa-user-plus"></i> New Enrollees</a></li>
+        <li><a href="subject.php"><i class="fas fa-book"></i> Subject</a></li>
+        <li><a href="courses.php"><i class="fas fa-graduation-cap"></i> Courses</a></li>
+        <li><a href="schedules.php"><i class="fas fa-calendar-alt"></i> Schedules</a></li>
+        <li><a href="students.php"><i class="fas fa-users"></i> Students</a></li>
+        <li><a href="professor.php"><i class="fas fa-chalkboard-teacher"></i> Professor</a></li>
+        <li><a href="set-semester.php"><i class="fas fa-cogs"></i> Set Semester</a></li>
+    </ul>
+</div>
--- admin/index.php
+++ admin/index.php
@@ -3,9 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
-    
-</body>
-</html>
+<?php
+include "include/sidebar.php";
+?>
+	<!-- CONTENT -->
+<section id="content">
+    <main>
+            <div class="left" style="margin-left: 290px">
+            <br>
+                <ul class="breadcrumb">
+                    <li>
+                        <a href="#">Dashboard /</a>
+                    </li>
+                    <li><i class='bx bx-chevron-right'></i></li>
+                    <li>
+                        <a class="active" href="#">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="statistics-box">
+            <div class="box-info">
+                <div class="box-item">
+                    <i class='bx bxs-user'></i>
+                    <div class="text">
+                        <h3>20</h3>
+                        <p>STUDENTS</p>
+                    </div>
+                </div>
+
+                <div class="box-item">
+                    <i class='bx bxs-user'></i>
+                    <div class="text">
+                        <h3>20</h3>
+                        <p>PROFESSORS</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</section>
