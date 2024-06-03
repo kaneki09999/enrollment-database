76c0239fefb5b7703b3235a3201cebb46900693a
kaneki09999 <tanjirowkamado@gmail.com>
2024-06-03 15:43:05 +0800

    new

--- admin/subject.php
+++ admin/subject.php
@@ -54,11 +54,10 @@
       <div class="container-fluid">
         <div class="row">
           <div class="col-12"> 
-            
-          <!-- Button trigger modal -->
+   
 
 <!-- DAGDAG NG ROOM NUMBER AND GAGAWIN TONG SCHEDULE -->
-            <!-- ADD MODAL -->
+            <!-- ADD MODAL here -->
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
