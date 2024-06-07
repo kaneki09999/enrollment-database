1eb98a37153f2ee90e680df08864b212a669ef92
kaneki09999 <tanjirowkamado@gmail.com>
2024-06-07 15:44:37 +0800

    update

--- admin/function/login.php
+++ admin/function/login.php
@@ -8,21 +8,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $username = mysqli_real_escape_string($conn, $username);
     
-    $query = "SELECT * FROM tbladmin WHERE username='$username'";
-    $result = mysqli_query($conn, $query);
+    $stmt = $conn->prepare("CALL SelectAdmin(?)");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-    if (mysqli_num_rows($result) == 1) {
-        $row = mysqli_fetch_assoc($result);
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
         if (password_verify($password, $row['password'])) {
             $_SESSION['id'] = $row['id'];
             $_SESSION['username'] = $row['username'];
             header("Location: ../home.php"); 
             exit();
         } else {
-            echo "Invalid username or password.";
+            echo "<script>
+                    alert('Invalid username or password.');
+                    window.location.href = '../index.php';
+                  </script>";
         }
     } else {
-        echo "Invalid username or password.";
+        echo "<script>
+        alert('Invalid username or password.');
+        window.location.href = '../index.php';
+      </script>";
     }
+
+    $stmt->close();
 }
+
+$conn->close();
 ?>
